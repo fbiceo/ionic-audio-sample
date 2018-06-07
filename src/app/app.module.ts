a6a2590a2a39d5ed55760f4e87218538253c7006
Shrek <shrek@itune.tw>
2018-06-07 15:44:05 +0800

    Sample Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicAudioModule, WebAudioProvider, CordovaMediaProvider, defaultAudioProviderFactory } from 'ionic-audio';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+/**
+ * Sample custom factory function to use with ionic-audio
+ */
+export function myCustomAudioProviderFactory() {
+  return (window.hasOwnProperty('cordova')) ? new CordovaMediaProvider() : new WebAudioProvider();
+}
 
+export function providers() {
+  return [
+    {provide: ErrorHandler, useClass: IonicErrorHandler}, 
+    SplashScreen, 
+    StatusBar
+  ];
+}
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +28,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicAudioModule.forRoot(defaultAudioProviderFactory), 
   ],
   bootstrap: [IonicApp],
   entryComponents: [
